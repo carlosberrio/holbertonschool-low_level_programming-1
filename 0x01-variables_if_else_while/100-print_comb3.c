99baa7534c7f21e7d314c570094294acc491fc87
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-02-07 00:00:56 +0000

    combo 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 8 ; i++)
+	{
+		for (j = i + 1 ; j <= 9 ; j++)
+		{
+			putchar(i + 48);
+			putchar(j + 48);
+			if (!(i == 8 && j == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
