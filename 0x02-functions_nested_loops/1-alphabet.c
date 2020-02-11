c820386b7d8ba6a0491f470a977585c065608e3d
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-02-11 13:37:49 +0000

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
