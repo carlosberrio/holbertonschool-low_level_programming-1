2befad03a5a5a0825da49ae58ea8ec38b365b154
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-02-11 23:43:01 +0000

    Suma de dos

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - function that add two integers and returns the result.
+ * @n: input integer
+ * @m: input integer
+ * Return: Always 1 (Success)
+ */
+
+int add(int n, int m)
+{
+	return (n + m);
+}
