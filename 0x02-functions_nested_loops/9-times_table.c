b284770923048c833ae5c6c14ef3fd1f324a6ad1
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-02-11 23:15:37 +0000

    Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,32 +8,34 @@
 
 void times_table(void)
 {
-/*
-	int col;
-	int c;
-	int out;
-	char numeros[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int n, m, out, dec, units;
 
-	for (col = 0 ; col < 10 ; col++)
+	for (n = 0 ; n < 10 ; n++)
 	{
-		for (c = 0 ; c < 10 ; c++)
-		{
-			out = numeros[0] * c;
-			_putchar('0' + out);
-		}
-		_putchar('\n');
-	}
-*/
-	int n, m, out;
-
-	for(n = 0 ; n < 10 ; n++)
-	{
-		for(m = 0 ; m < 10 ; m++)
+		for (m = 0 ; m < 10 ; m++)
 		{
 			out = n * m;
-			_putchar(out + '0');
+			if (out > 9)
+			{
+				dec = out / 10;
+				units = out % 10;
+				_putchar(dec + '0');
+				_putchar(units + '0');
+			}
+			else if (out < 99)
+			{
+				_putchar(out + '0');
+			}
+			if (m < 9)
+			{
+				_putchar(44);
+				_putchar(' ');
+				if (n * (m + 1) < 10)
+				{
+					_putchar(' ');
+				}
+			}
 		}
 		_putchar('\n');
 	}
-
 }
