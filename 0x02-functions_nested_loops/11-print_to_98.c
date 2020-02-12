b013e3c0eb39daf136e06304664295b3ade8349f
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-02-12 00:22:19 +0000

    Printint until 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - all natural numbers from n to 98, followed by a new line
+ * @n: input integer
+ * Return: Always 1 (Success)
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (; n <= 98 ; n++)
+		{
+			printf("%d", n);
+			if (n < 98)
+			{
+				printf(",");
+			}
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (; n >= 98 ; n--)
+		{
+			printf("%d", n);
+			if (n > 98)
+			{
+				printf(",");
+			}
+		}
+		printf("\n");
+	}
+	else
+	{
+		printf("%d\n", n);
+	}
+}
