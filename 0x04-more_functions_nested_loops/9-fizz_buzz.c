4c95036e7cdfc6ce18d49e19c51efb2b21323103
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-02-13 21:19:30 +0000

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1 ; num <= 100 ; ++num)
+	{
+		if (num % 15 == 0)
+		{
+			printf("Fizz Buzz");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+		printf("%d", num);
+		}
+		if(num != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
