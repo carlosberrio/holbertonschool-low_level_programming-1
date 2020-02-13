e4c948768c5dde7f1c694a98c165ec7ae599a074
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-02-13 20:19:45 +0000

    Printing lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+A#include "holberton.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * @lines: lines to print
+ * Return: Always 0 (Success)
+ */
+
+void print_line(int lines)
+{
+	int counter;
+
+	for (counter = 1 ; counter <= lines ; counter++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
