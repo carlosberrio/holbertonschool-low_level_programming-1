d4bd18e7a7eaa37e6d52f44fcb72271fa1b6fd03
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-02-13 15:42:46 +0000

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - function that checks for a digit.
+ * @c: input integer
+ * Return: Always 1 (Success)
+ */
+
+int _isdigit(int c)
+{
+
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
