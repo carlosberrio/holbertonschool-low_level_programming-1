19b2b365a1abd281d870972ddabe4703e73115fc
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-02-18 13:51:44 +0000

    Reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - takes a pointer to an int as parameter and updates the value.
- * @*n: pointer input.
+ * @n: pointer input.
  * Return: Always 0.
  */
 void reset_to_98(int *n)
