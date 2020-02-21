d6d098db1923d0f6cc92d47ce9b6291abd9bd38a
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-02-21 04:51:36 +0000

    Capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * *cap_string - Function that capitalize
+ * @str: string
+ * Return: string changed to uppercase
+ */
+char *cap_string(char *str)
+{
+	int ch, c;
+
+	char sign[] = {' ', '\t', '\n', ',', ';', '.', '!','?',
+		       '"', '(', ')', '{', '}'};
+
+	if (str[0] >= 97 && str[0] <= 122)
+	{
+		str[0] -= 32;
+	}
+
+	for (ch = 0 ; str[ch] != '\0' ; ch++)
+	{
+		for (c = 0 ; c < 12 ; c++)
+		{
+			if (str[ch] == sign[c])
+			{
+				if (str[ch + 1] >= 97 && str[ch + 1] <= 122)
+				{
+					str[ch + 1] -= 32;
+				}
+			}
+		}
+
+	}
+	return (str);
+}
