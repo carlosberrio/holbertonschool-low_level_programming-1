a2c555f2e7ed2855cbb5af94420f89ea164d672c
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-02-27 01:18:05 +0000

    Pow with recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,6 +5,12 @@
  *@x: base number
  *@y: exponent number
  * Return: -1 if 'y' is lower than 0 or the math operation
+ * better way due to less iterations
+ * int pow = power(x, n / 2);
+ * if (n & 1) // if y is odd
+ * re.turn x * pow * pow;
+ * else 'y' is even
+ * re.turn pow * pow;
  */
 int _pow_recursion(int x, int y)
 {
