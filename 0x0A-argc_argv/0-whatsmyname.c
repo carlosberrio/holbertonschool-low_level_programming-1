f8882140640c9e139b2cd879505fec8fd8779128
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-03-01 00:28:26 +0000

    Argc/Argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main (__attribute__ ((unused)) int argc, char *argv[])
+/**
+ * main - Write a program that prints its name, followed by a new line.
+ * @argc: arguments count.
+ * @argv: array of strings.
+ * Return: Always 0.
+ */
+
+int main(__attribute__ ((unused)) int argc, char *argv[])
 {
-	printf ("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
