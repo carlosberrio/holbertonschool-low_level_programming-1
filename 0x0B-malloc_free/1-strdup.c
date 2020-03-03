41dd32c2ed630dd51f3759f85df1842087dcb6c0
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-03-03 21:39:40 +0000

    Copying a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -46,9 +46,10 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
-	for (i = 0 ; i <= size ; i++)
+	for (i = 0 ; i < size ; i++)
 	{
 		new_arr[i] = str[i];
 	}
+	new_arr[i] = '\0';
 	return (new_arr);
 }
