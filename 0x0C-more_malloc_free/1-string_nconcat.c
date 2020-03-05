978bc25cf790b0d968b8dedb13dff0623258b1b9
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-03-05 19:58:30 +0000

    Concat String

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,26 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <string.h>
+
+int _strlen(char *s);
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string input.
+ * Return: length of an string.
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
 
 /**
  * *string_nconcat - concatenates two strings
@@ -14,28 +34,33 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int i, s1_size;
+	unsigned int i, s1_size, s2_size;
 
 	if (!s1)
 		s1 = "";
 	if (!s2)
 		s2 = "";
 
-	s1_size = strlen(s1);
+	s1_size = _strlen(s1);
+	s2_size = _strlen(s2);
 
-	str = malloc(sizeof(s1_size) + n + 1);
+	if (n > s2_size)
+		n = s2_size;
+
+	str = malloc(sizeof(char) * (s1_size + n) + 1);
 
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; i < (s1_size + n); i++)
+	for (i = 0; i < s1_size; i++)
 	{
-		if (i < s1_size)
-			str[i] = s1[i];
-		else
-			str[i] = s2[i - s1_size];
+		str[i] = s1[i];
 	}
-	str[i + 1] = '\0';
+	for (i = 0; i < n; i++)
+	{
+		str[s1_size + i] = s2[i];
+	}
+	str[s1_size + i + 1] = '\0';
 
 	return (str);
 }
