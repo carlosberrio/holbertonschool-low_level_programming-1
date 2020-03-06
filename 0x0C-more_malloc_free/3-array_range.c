66052ed7fe55da2e9d7046afe0ef8ad887a54aa1
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-03-06 02:06:37 +0000

    Array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -21,8 +21,11 @@ int *array_range(int min, int max)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = min; i <= max; i++)
-		ptr[i] = i;
+	for (i = 0; i <= (max - min); i++)
+	{
+		ptr[i] = min;
+		++min;
+	}
 
 	return (ptr);
 }
