9f95e94479dc9114618f21e9d22e128772b0ff57
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-03-09 19:43:22 +0000

    Functions like macros

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
-#ifndef ABS
+#ifndef ABS_X
+#define ABS_X
 #define ABS(x) ((x) > (0) ? (x) : (x) * (-1))
 #endif /* ABS(x) */
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,4 @@
+#ifndef SUM_C_D
+#define SUM_C_D
+#define SUM(c, d) ((c) + (d))
+#endif /* SUM_C_D */
