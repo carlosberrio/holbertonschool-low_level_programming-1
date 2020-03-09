94012cb77c1ce1011a8ea76932a94ff2ff28a271
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-03-09 21:57:48 +0000

    dog header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - get info about dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: data structure to store info about dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
