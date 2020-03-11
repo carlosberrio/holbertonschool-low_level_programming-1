665f3bd8549f2c0e88d8660fa21ede4d85eb4ee5
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-03-11 16:54:33 +0000

    Function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,8 +12,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t c;
 
-	for (c = 0; c < size; c++)
+	if (array && action)
 	{
-		action(array[c]);
+		for (c = 0; c < size; c++)
+		{
+			action(array[c]);
+		}
 	}
 }
