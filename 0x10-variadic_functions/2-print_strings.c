30cd018ea4df3969e36f89411544931918d57ee3
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-03-14 19:40:17 +0000

    Fixing tasks

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", string);
 
-			if (c < (n - 1))
+			if (c < (n - 1) && separator)
 			{
 				printf("%s", separator);
 			}
