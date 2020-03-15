79f3dda10a8a4889b3c09e9ad148146a82e074e7
Tatiana Orejuela Zapata <tatianaorezap@gmail.com>
2020-03-15 01:16:30 +0000

    Variadic Mains

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_all("ceis", 'H', 0, "lberton");
+	print_all("seic", "HOLI", 9, "O");
+	print_all("ceis", NULL, 9, NULL);
+	return (0);
+}
