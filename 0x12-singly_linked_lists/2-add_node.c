6214b1fd7e956cf9aa6846cd9d6363543c0ef137
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-03-20 00:00:26 +0000

    Linked Lists Task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: first element of the list
+ * @str: string to be copied
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
