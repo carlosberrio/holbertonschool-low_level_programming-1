aa25664dc2a203974c1d15f2dda000cbf4c302c1
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-03-20 04:08:43 +0000

    Adding Advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - Print something before main
+ *
+ * Return: Always 0.
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
