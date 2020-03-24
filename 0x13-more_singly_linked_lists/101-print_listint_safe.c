b8086027eefae33ffdf9b72fcfdaa8f2111b7e6e
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-03-24 23:24:09 +0000

    Adding Task No. 101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Function that prints all the elements of a list_t list.
+ * @head: head of the list
+ * Return: Number of nodes
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes = 0;
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)&head->n, head->n);
+
+/*check directions to break the infinite loop */
+		if ((void *)&head->n < (void *)&head->next->n)
+		{
+			printf("-> [%p] %d\n", (void *)&head->next->n,
+			       head->next->n);
+			break;
+		}
+
+		head = head->next;
+		nodes++;
+	}
+	return (nodes);
+}
