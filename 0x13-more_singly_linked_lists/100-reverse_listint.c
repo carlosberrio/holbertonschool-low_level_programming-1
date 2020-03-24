6b6d279dcdfd9554fd80d5474ee45e48ece1197b
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-03-24 20:54:14 +0000

    Reverse a list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,26 +10,21 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *first, *rest;
 
-	if (!(*head))
+	if (head == NULL)
 		return (NULL);
 
 	first = (*head);
 	rest  = first->next;
 
-/* if list has only one node */
-
 	if (rest == NULL)
 		return (rest);
 
-/* reverse the rest list and put the first element at the end */
 	reverse_listint(&rest);
 
 	first->next->next = first;
 
-	/* tricky step -- see the diagram */
 	first->next  = NULL;
 
-	/* fix the head pointer */
 	(*head) = rest;
 
 	return (*head);
