c96cff5e2c6c58afda6558963eee16885e0d0f4f
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-03-31 01:54:37 +0000

    Adding task 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * print_multiples - computes and prints the sum of all the multiples of 3 or 5
+ * main - computes and prints the sum of all the multiples of 3 or 5
  * below 1024 (excluded), followed by a new line.
  * Return: Nothing
  */
