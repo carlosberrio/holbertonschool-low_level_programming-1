2a63c66da968472ee55fb0e31e2605227f4bf9a7
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-03-31 22:55:05 +0000

    Adding Task No. 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,13 +15,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[size] != '\0')
-		size++;
-
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
+	if (text_content)
+	{
+		while (text_content[size] != '\0')
+			size++;
+	}
 
 	w_bytes = write(fd, text_content, size);
 
