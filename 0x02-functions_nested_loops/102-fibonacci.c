88f100615cb4683b79b170890c0a04f98da1d64f
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-03-31 02:41:20 +0000

    Adding Task 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Write a program that prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ * Return: Nothing
+ */
+
+int main(void)
+{
+	int n = 0;
+	long long int prev = 0, res = 1, sum = 0;
+
+	while (n < 50)
+	{
+		sum = prev + res;
+		prev = res;
+		res = sum;
+		printf("%lld", sum);
+
+		if (n < 49)
+			printf(", ");
+
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
