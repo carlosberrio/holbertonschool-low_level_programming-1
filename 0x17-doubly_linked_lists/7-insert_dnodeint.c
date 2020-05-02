c93a6157df9c87effd821b26845da09571067af3
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-05-02 06:44:19 +0000

    fix return

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,6 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new_node->next = (*h)->next;
 			new_node->prev = (*h)->prev;
 			(*h)->next = new_node;
+			return (new_node);
 		}
 		h = &(*h)->next;
 		counter++;
